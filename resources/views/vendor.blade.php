1426b496a0860dc3310371c6ab8bb388e90a8c75
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2021-07-29 20:32:58 +0530

    Add placeholders to input fields

--- resources/views/vendor.blade.php
+++ resources/views/vendor.blade.php
@@ -10,11 +10,11 @@
                 <form id="formTopping">
                     <div class="col mt-4">
                         <label for="toppingName">Name</label>
-                        <input type="text" class="form-control" id="toppingName" name="toppingName"/>
+                        <input type="text" class="form-control" id="toppingName" name="toppingName" placeholder="Chocolate"/>
                     </div>
                     <div class="col mt-3">
                         <label for="toppingPrice">Price</label>
-                        <input type="text" class="form-control" id="toppingPrice" name="toppingPrice" />
+                        <input type="text" class="form-control" id="toppingPrice" name="toppingPrice" placeholder="50"/>
                     </div>
                     <button type="button" class="btn btn-primary mt-4 mb-4" id="btnAddTopping" onclick="onclick_add_topping();">Add Topping</button>
                 </form>
