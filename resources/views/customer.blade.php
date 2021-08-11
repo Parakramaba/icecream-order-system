651b70af91a6f71665b9809841977c59c9c2e9db
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2021-08-11 18:57:55 +0530

    Update the Customer Home page UI

--- resources/views/customer.blade.php
+++ resources/views/customer.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.layout')
+@extends('layouts.customer')
 
 @section('content')
 <script>
@@ -8,42 +8,52 @@
 </script>
 <div class="container">
     <div class="row">
-        <div class="col-12 border">
-            <h3 class="font-weight-bold">Make Your Order</h3>
-            <form id="formOrder">
-                <div class="mt-3">
-                    <select name="orderType" id="orderType" class="form-control" onchange="onchange_order();">
-                        <option disabled hidden selected>Please Select Your Order Type</option>
-                        <option value="1">Plain Ice Cream</option>
-                        <option value="2">Ice Cream with Toppings</option>
-                    </select>
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="font-weight-bold text-center">Make Your Order</h3>
+                    <form id="formOrder">
+                        {{-- <div class="col-8 text-center">
+                            <div class="row"> --}}
+                                <div class="mt-3">
+                                    <select name="orderType" id="orderType" class="form-control" onchange="onchange_order();">
+                                        <option disabled hidden selected>Please Select Your Order Type</option>
+                                        <option value="1">Plain Ice Cream</option>
+                                        <option value="2">Ice Cream with Toppings</option>
+                                    </select>
+                                </div>
+                                <div class="mt-3" id="divToppings">
+                                    <label for="toppings[]" class="form-label">Choose your Toppings</label><br>
+                                    @foreach ($toppings as $topping)
+                                        <input type="checkbox" name="toppings[]" id="toppings" value="{{ $topping->name }}">  {{ $topping->name }} - {{ $topping->price }}LKR<br>
+                                    @endforeach
+                                </div>
+                            {{-- </div>
+                        </div> --}}
+                    </form>
+                    <div class="d-grid col-4 mx-auto mt-4 mb-4">
+                        <button type="button"  class="btn btn-primary" id="btnPlaceOrder" onclick="onclick_place_order();">Place Order</button>
+                    </div>
                 </div>
-                <div class="mt-3" id="divToppings">
-                    <label for="toppings[]" class="form-label">Choose your Toppings</label><br>
-                    @foreach ($toppings as $topping)
-                        <input type="checkbox" name="toppings[]" id="toppings" value="{{ $topping->name }}">  {{ $topping->name }} - {{ $topping->price }}LKR<br>
-                    @endforeach
-                </div>
-                <button type="button" class="btn btn-primary mt-4 mb-4" id="btnPlaceOrder" onclick="onclick_place_order();">Place Order</button>
-            </form>
+            </div>
         </div>
         <div class="col-12 mt-5" id="divOrderDetails">
             <div class="row">
                 <div class="col-md-6 mb-5">
                     <div class="card h-100">
-                    <div class="card-body">
-                        <h5 class="card-title">Your Order Details</h5>
-                        <p></p>
-                        <button class="btn btn-primary mb-4" onclick="onclick_print_details();">Print</button>
-                    </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Your Order Details</h5>
+                            <p></p>
+                            <button class="btn btn-primary mb-4" onclick="onclick_print_details();">Print</button>
+                        </div>
                     </div>
                 </div>
-                <div class="col-sm-6 mb-5">
+                <div class="col-md-6 mb-5">
                     <div class="card h-100">
-                    <div class="card-body">
-                        <h5 class="card-title">Final Price</h5>
-                        <button class="btn btn-primary mb-4" onclick="onclick_print_price();">Print</button>
-                    </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Final Price</h5>
+                            <button class="btn btn-primary mb-4" onclick="onclick_print_price();">Print</button>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ resources/views/layouts/customer.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ice Cream Flavours-Order</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="keyword" content="Ice Cream, Ice Cream Order, Chocolate, Topping">
+    
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- / Bootstrap -->
+    
+    <!-- jQuery --> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- FontAwesome --> <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <!-- CSS --> <link rel="stylesheet" href="{{ asset('css/customer.css') }}">
+    
+</head>
+
+<body>
+    <div class="bg-dark bg-gradient">
+        {{-- Header --}}
+        <header>
+            <div class="container">
+                <ul class="nav nav-pills justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link bg-dark text-light mt-2 text-uppercase" href="{{ route('vendor.home') }}"><i class="fas fa-user"></i> Vendor Sign In</a>
+                    </li>
+                </ul>
+                <div class="justify-content-center mt-2">
+                    <div class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="{{ url('img/header-1.jpg') }}" class="d-block w-100" alt="Ice Cream 1" />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="{{ url('img/header-2.jpg') }}" class="d-block w-100" alt="Ice Cream 2" />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="{{ url('img/header-3.jpg') }}" class="d-block w-100" alt="Ice Cream 3" />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="{{ url('img/header-4.jpg') }}" class="d-block w-100" alt="Ice Cream 4" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        {{-- /Header --}}
+
+        {{-- Content --}}
+        <div class="mt-5">
+            @yield('content')
+        </div>
+        {{-- /Content --}}
+    </div>
+
+    {{-- Footer --}}
+    <footer class="bg-secondary bg-gradient text-center text-dark p-2">
+        <div class="text-center">
+            <a href="" class="text-dark m-2" style="font-size: 35px;"><i class="fab fa-whatsapp"></i></a>
+            <a href="" class="text-dark m-2" style="font-size: 35px;"><i class="fab fa-facebook-square"></i></a>
+            <a href="" class="text-dark m-2" style="font-size: 35px;"><i class="fab fa-linkedin"></i></a>
+            <a href="" class="text-dark m-2" style="font-size: 35px;"><i class="fab fa-twitter-square"></i></a>
+            <a href="" class="text-dark m-2" style="font-size: 35px;"><i class="fab fa-viber"></i></a>
+        </div>
+        Copyright &copy; 2021 @ IceCreamFavour
+    </footer>
+    {{-- /Footer --}}
+</body>
+</html>
