dd2dc7a600805823fda8c801341e37b6b171e864
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2021-09-07 17:30:03 +0530

    Add YajraDataTables

--- resources/views/layouts/vendor.blade.php
+++ resources/views/layouts/vendor.blade.php
@@ -1,22 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Ice Cream Flavours-Vendor</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="keyword" content="Ice Cream, Ice Cream Order, Chocolate, Topping">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="keyword" content="Ice Cream, Ice Cream Order, Chocolate, Topping" />
 
     <!-- Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous" />
     <!-- / Bootstrap -->
     
     <!-- jQuery --> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- jQuery DataTables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.1/css/jquery.dataTables.min.css" />
+    <script src="https://cdn.datatables.net/1.11.1/js/jquery.dataTables.min.js"></script>
+    <!-- jQuery DataTables -->
+
     <!-- FontAwesome --> <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-    <!-- LineAwesome --> <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-    <!-- SweetAlert --> <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <!--CSS --> <link rel="stylesheet" href="{{ asset('css/vendor.css') }}">
+    <!-- LineAwesome --> <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" />
+    <!-- SweetAlert --> <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!--CSS --> <link rel="stylesheet" href="{{ asset('css/vendor.css') }}" />
 
 </head>
 <body>
