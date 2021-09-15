b75c736685e632e895f2fd267ced48870d73d0ff
Parakramaba <74170793+Parakramaba@users.noreply.github.com>
2021-09-15 17:11:53 +0530

    Update SweetAlerts with Error Messages

--- public/js/sweetalert.js
+++ public/js/sweetalert.js
@@ -13,6 +13,39 @@ const SwalError = Swal.mixin({
     iconColor: color_danger,
     confirmButtonColor: color_danger,
     allowOutsideClick: false,
-});
+})
+
+const SwalSystemError = Swal.mixin({
+    icon: 'error',
+    iconColor: color_danger,
+    title: 'System Error!',
+    text: 'Please refresh the page and try again. Still cannot proceed please contact the system administrator',
+    confirmButtonColor: color_danger,
+    allowOutsideClick: false,
+})
+
+const SwalInvalidIdError = Swal.mixin({
+    icon: 'error',
+    iconColor: color_danger,
+    title: 'Invalid Id',
+    text: 'Please refresh the page and try again. Still cannot proceed please contact the system administrator',
+    confirmButtonColor: color_danger,
+    allowOutsideClick: false,
+})
 // /ERROR
+
+// NOTIFICATION
+// const SwalNotificationWarningAutoClose = Swal.mixin({
+//     icon: 'warning',
+//     iconColor: color_warning,
+//     titleText: 'Id Not Found',
+//     text: 'Please refresh the page and try again. Still cannot proceed please contact the system administrator',
+//     showConfirmButton: false,
+//     toast: true,
+//     position: 'top-end',
+//     timer: 12000,
+//     timerProgressBar: true,
+// })
+// /NOTIFICATION
+
 // /SWEETALERT
